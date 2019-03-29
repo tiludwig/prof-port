c1d6ddf29bbdee07c347920a407b27581f6a232f
Tim Ludwig <ludwig.tim@t-online.de>
2019-03-29 12:46:34 +0100

    Add new packet communicator

---
+++ src/Core/Communicator/PacketCommunicator.h
@@ -0,0 +1,37 @@
+/*
+ * PacketCommunicator.h
+ *
+ *  Created on: 29.03.2019
+ *      Author: Tim
+ */
+
+#ifndef CORE_COMMUNICATOR_PACKETCOMMUNICATOR_H_
+#define CORE_COMMUNICATOR_PACKETCOMMUNICATOR_H_
+
+#include <Components/ComLink/IComLink.hpp>
+#include <Core/DataLink/PacketProtocol.h>
+#include <Core/DataLink/Packet.h>
+#include <Core/DataLink/PacketReceiver.h>
+
+class PacketCommunicator
+{
+private:
+	IComLink* comDriver;
+	PacketReceiver* receiver;
+private:
+	void sendStartSymbol();
+	void send(char* buffer, uint16_t size);
+	void sendByte(char value);
+public:
+	PacketCommunicator(IComLink* link, PacketReceiver* recv);
+
+	int8_t calculateChecksum(packet_t& packet);
+	void sendPacket(packet_t& packet);
+	void receivePacket(packet_t& packet);
+
+
+	packet_t waitForRequest();
+	void sendResponse(packet_t& response);
+};
+
+#endif /* CORE_COMMUNICATOR_PACKETCOMMUNICATOR_H_ */
